e557789deaec6cae64916b1ef8bca89224455e59
abdulArif <abdul18ti@mahasiswa.pcr.ac.id>
2022-04-26 14:38:50 +0700

    update table kegiatan

--- resources/views/RW/Kegiatan/table_kegiatan.blade.php
+++ resources/views/RW/Kegiatan/table_kegiatan.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.main')
-
-@section('container')
+@section('container') 
 <div class="page-body">
     <div class="container-fluid">
         <div class="page-header">
-            <div class="row">
-            </div>
+            <div class="row"></div>
         </div>
     </div>
     <div class="container-fluid">
@@ -18,14 +16,11 @@
                                 <h5>Data kegiatan</h5>
                             </div>
                             <div class="col-3">
-                                <div class="bookmark">
-                                    <a class="btn btn-primary btn-lg" data-bs-toggle="modal"
+                                <div class="bookmark"><a class="btn btn-primary btn-lg" data-bs-toggle="modal"
                                         data-bs-target="#exampleModalgetbootstrapCREATE"><span
-                                            class="fa fa-plus-square"></span> Tambah Data</a>
-                                </div>
+                                            class="fa fa-plus-square"></span>Tambah Data</a></div>
                             </div>
                         </div>
-
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
@@ -47,14 +42,15 @@
                                         <td>tes</td>
                                         <td>tes</td>
                                         <td>tes</td>
-                                        <td>
-                                            <a href="" class="btn btn-success btn-sm p-2" data-container="body"
-                                            data-bs-toggle="modal" data-bs-target=".bd-example-modal-lg" data-bs-placement="top" title="Detail"><span
-                                                    class="fa fa-list"></span></a>
-                                            <a href="" class="btn btn-secondary btn-sm p-2" data-container="body" data-bs-placement="top" data-bs-toggle="modal"
+                                        <td><a href="" class="btn btn-success btn-sm p-2" data-container="body"
+                                                data-bs-toggle="modal" data-bs-target=".bd-example-modal-lg"
+                                                data-bs-placement="top" title="Detail"><span class="fa fa-list"></span></a><a href="" class="btn btn-secondary btn-sm p-2" data-container="body" data-bs-placement="top" data-bs-toggle="modal"
                                                 data-bs-target="#exampleModalgetbootstrapEDIT" title="Edit"><span
                                                     class="fa fa-pencil"></span></a>
-                                        </td>
+                                                    {{-- <div
+                                                class="btn btn-danger btn-sm p-2" type="button" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-5']);"><span
+                                                    class="fa fa-trash"></span></div></td> --}}
+                                                        <button class="btn btn-warning sweet-5" type="button" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-5']);">Warning alert</button>
                                     </tr>
                                 </tbody>
                                 <tr>
@@ -72,96 +68,79 @@
             </div>
         </div>
     </div>
-</div>
-{{-- modal tambah --}}
-<div class="modal fade" id="exampleModalgetbootstrapCREATE" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
+</div> 
+{{-- modal tambah  --}}
+<div class="modal fade" id="exampleModalgetbootstrapCREATE" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Tambah data kegiatan</h5>
-                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h5 class="modal-title"> Tambah data kegiatan</h5><button class="btn-close" type="button"
+                    data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form>
+                    <div class="mb-3"><label class="col-form-label">Nama kegiatan:</label><input class="form-control"
+                            type="text" name="nama_kegiatan"></div>
+                    <div class="mb-3"><label class="col-form-label" for="message-text">Deskripsi
+                            kegiatan:</label><textarea class="form-control" rows="9"></textarea></div>
                     <div class="mb-3">
-                        <label class="col-form-label">Nama kegiatan:</label>
-                        <input class="form-control" type="text" name="nama_kegiatan">
+                        <div class="col-form-label"><img class="img-preview img-fluid mb-3 col-sm-5"><input
+                                class="form-control" name="foto_pengumuman" onchange="previewImage()" id="image"
+                                type="file" /></div>
                     </div>
                     <div class="mb-3">
-                        <label class="col-form-label" for="message-text">Deskripsi kegiatan:</label>
-                        <textarea class="form-control" rows="9"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <div class="col-form-label">
-                            <img class="img-preview img-fluid mb-3 col-sm-5">
-                            <input class="form-control" name="foto_pengumuman" onchange="previewImage()" id="image"
-                                type="file" />
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <div class="col-form-label">
-                            <input class="form-control digits" id="example-datetime-local-input" type="datetime-local"
-                                name="tgl_terbit" value="2018-01-19T18:45:00" />
-                        </div>
+                        <div class="col-form-label"><input class="form-control digits" id="example-datetime-local-input"
+                                type="datetime-local" name="tgl_terbit" value="2018-01-19T18:45:00" /></div>
                     </div>
                 </form>
             </div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Batal</button>
-                <button class="btn btn-primary" type="button">Tambah</button>
+            <div class="modal-footer"><button class="btn btn-secondary" type="button"
+                    data-bs-dismiss="modal">Batal</button><button class="btn btn-primary" type="button">Tambah</button>
             </div>
         </div>
     </div>
-</div>
+</div> 
 {{-- modal edit --}}
-<div class="modal fade" id="exampleModalgetbootstrapEDIT" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
+<div class="modal fade" id="exampleModalgetbootstrapEDIT" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Edit data kegiatan</h5>
-                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h5 class="modal-title">Edit data kegiatan</h5><button class="btn-close" type="button"
+                    data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form>
+                    <div class="mb-3"><label class="col-form-label">Nama kegiatan:</label><input class="form-control"
+                            type="text" name="nama_kegiatan"></div>
+                    <div class="mb-3"><label class="col-form-label" for="message-text">Deskripsi
+                            kegiatan:</label><textarea class="form-control" rows="9"></textarea></div>
                     <div class="mb-3">
-                        <label class="col-form-label">Nama kegiatan:</label>
-                        <input class="form-control" type="text" name="nama_kegiatan">
+                        <div class="col-form-label"><img class="img-preview img-fluid mb-3 col-sm-5"><input
+                                class="form-control" name="foto_pengumuman" onchange="previewImage()" id="image"
+                                type="file" /></div>
                     </div>
                     <div class="mb-3">
-                        <label class="col-form-label" for="message-text">Deskripsi kegiatan:</label>
-                        <textarea class="form-control" rows="9"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <div class="col-form-label">
-                            <img class="img-preview img-fluid mb-3 col-sm-5">
-                            <input class="form-control" name="foto_pengumuman" onchange="previewImage()" id="image"
-                                type="file" />
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <div class="col-form-label">
-                            <input class="form-control digits" id="example-datetime-local-input" type="datetime-local"
-                                name="tgl_terbit" value="2018-01-19T18:45:00" />
-                        </div>
+                        <div class="col-form-label"><input class="form-control digits" id="example-datetime-local-input"
+                                type="datetime-local" name="tgl_terbit" value="2018-01-19T18:45:00" /></div>
                     </div>
                 </form>
             </div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Batal</button>
-                <button class="btn btn-primary" type="button">Tambah</button>
+            <div class="modal-footer"><button class="btn btn-secondary" type="button"
+                    data-bs-dismiss="modal">Batal</button><button class="btn btn-primary" type="button">Tambah</button>
             </div>
         </div>
     </div>
 </div>
 {{-- modal detail --}}
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="myLargeModalLabel">Detail kegiatan</h4>
-                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h4 class="modal-title" id="myLargeModalLabel">Detail kegiatan</h4><button class="btn-close"
+                    type="button" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div class="container-fluid">
@@ -170,20 +149,22 @@
                             <div class="blog-single">
                                 <div class="blog-box blog-details">
                                     <div class="banner-wrraper"><img class="img-fluid w-100 bg-img-cover"
-                                            src="{{asset('assets/images/blog/blog-single.jpg')}}" alt="blog-main" /></div>
+                                            src="{{asset('assets/images/blog/blog-single.jpg')}}" alt="blog-main" />
+                                    </div>
                                     <div class="card">
                                         <div class="card-body">
                                             <div class="blog-details">
-                                                <h4>
-                                                    Judul.
-                                                </h4>
+                                                <h4>Judul. </h4>
                                                 <ul class="blog-social">
                                                     <li>Kategori</li>
                                                 </ul>
                                                 <div class="single-blog-content-top">
-                                                    <p>
-                                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, accusamus veniam numquam dignissimos, voluptatem excepturi quo nam ducimus dolor repellat non nihil debitis. Similique quam, totam ut minus quidem doloremque!
-                                                    </p>
+                                                    <p>Lorem ipsum dolor sit amet consectetur,
+                                                        adipisicing elit. Quaerat,
+                                                        accusamus veniam numquam dignissimos,
+                                                        voluptatem excepturi quo nam ducimus dolor repellat non nihil
+                                                        debitis. Similique quam,
+                                                        totam ut minus quidem doloremque ! </p>
                                                 </div>
                                             </div>
                                         </div>
