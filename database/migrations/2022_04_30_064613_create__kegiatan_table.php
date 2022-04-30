7a2c18d13865179144810824fc7fd3edc6a56a84
abdulArif <abdul18ti@mahasiswa.pcr.ac.id>
2022-04-30 13:54:27 +0700

    Migration Kegiatan

---
+++ database/migrations/2022_04_30_064613_create__kegiatan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKegiatanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_kegiatan', function (Blueprint $table) {
+            $table->id('id_kegiatan');
+            $table->string('nama_kegiatan');
+            $table->text('isi_kegiatan');
+            $table->string('foto_kegiatan')->nullable();
+            $table->integer('status_kegiatan');
+            $table->bigInteger('tgl_upload_kegiatan');
+            $table->bigInteger('tgl_mulai_kegiatan');
+            $table->bigInteger('tgl_selesai_kegiatan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_kegiatan');
+    }
+}
