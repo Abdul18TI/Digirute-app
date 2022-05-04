0f77957db7b42a7ab10b22a4de0e0ebfc83b9185
Andra Hotmartua Al Hafiz HSB <52515217+andrahafiz@users.noreply.github.com>
2022-05-04 19:40:50 +0700

    Templating

--- resources/views/partials/sidebar-rt.blade.php
+++ resources/views/partials/sidebar-rt.blade.php
@@ -3,10 +3,10 @@
     <!-- Page Sidebar Start-->
     <header class="main-nav">
         <div class="sidebar-user text-center"><a class="setting-primary" href="javascript:void(0)"><i data-feather="settings"></i></a><img class="img-90 rounded-circle" src="assets/images/dashboard/1.png" alt="">
-            <div class="badge-bottom"><span class="badge badge-primary">RW</span></div><a href="user-profile.html">
+            <div class="badge-bottom"><span class="badge badge-primary">RT</span></div><a href="user-profile.html">
                 <h6 class="mt-3 f-14 f-w-600">Sahid</h6>
             </a>
-            <p class="mb-0 font-roboto">RW 04</p>
+            <p class="mb-0 font-roboto">RT 01</p>
             <!-- <ul>
                 <li><span><span class="counter">19.8</span>k</span>
                     <p>Follow</p>
