d14f99b44433a6df1d6a7c09e3c2f548f0412508
Andra Hotmartua Al Hafiz HSB <52515217+andrahafiz@users.noreply.github.com>
2022-05-30 05:53:15 +0700

    Update Helpers.php

--- app/Helper/Helpers.php
+++ app/Helper/Helpers.php
@@ -1,17 +1,44 @@
 <?php
 
-namespace App\Helper;
+// namespace App\Helper;
 
 use App\Models\Pekerjaan;
 
 
 
-class Helpers
+// class Helpers
+// {
+#Get Category Data From Category Model
+function get_catdata()
 {
-    #Get Category Data From Category Model
-    static function get_catdata()
-    {
-        $catdata = Pekerjaan::all();
-        return $catdata;
-    }
+    $catdata = Pekerjaan::all();
+    return $catdata;
 }
+
+function FormatHP($nomorhp)
+{
+    //Terlebih dahulu kita trim dl
+    $nomorhp = trim($nomorhp);
+    //bersihkan dari karakter yang tidak perlu
+    $nomorhp = strip_tags($nomorhp);
+    // Berishkan dari spasi
+    $nomorhp = str_replace(" ", "", $nomorhp);
+    // bersihkan dari bentuk seperti  (022) 66677788
+    $nomorhp = str_replace("(", "", $nomorhp);
+    // bersihkan dari format yang ada titik seperti 0811.222.333.4
+    $nomorhp = str_replace(".", "", $nomorhp);
+
+    //cek apakah mengandung karakter + dan 0-9
+    if (!preg_match('/[^+0-9]/', trim($nomorhp))) {
+        // cek apakah no hp karakter 1-3 adalah +62
+        if (substr(trim($nomorhp), 0, 3) == '+62') {
+            $nomorhp = trim($nomorhp);
+        }
+        // cek apakah no hp karakter 1 adalah 0
+        elseif (substr($nomorhp, 0, 1) == '0') {
+            $nomorhp = '+62' . substr($nomorhp, 1);
+        }
+    }
+    return $nomorhp;
+}
+// }
